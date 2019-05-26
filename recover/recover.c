d5592541b07467dd9cb7353ad9b26aaf9ddbbfa3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-26 18:55:40 +0000

    first restraint

---
+++ recover/recover.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: \"./recover <image>\" should have exactly one prompt after recover\n");
+        return 1;
+    }
+
+}
